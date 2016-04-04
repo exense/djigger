241829f1d04bc990164bbf5a8b7ca274ae574c42
dcransac <dcransac@gmail.com>
2016-04-04 08:50:18 +0200

    Fixed server stop, added node split via percentage-methods

--- collector/src/main/java/io/djigger/collector/test/BasicJMXJVM.java
+++ collector/src/main/java/io/djigger/collector/test/BasicJMXJVM.java
@@ -40,32 +40,65 @@ public class BasicJMXJVM {
 
 		// Programmatically open JMX channel
 		BasicJMXJVM brjvm_1 = new BasicJMXJVM();
+		
+		JMXConnectorServer srv = null;
 		try {
-			brjvm_1.createJmxConnectorServer(9878);
+			srv = brjvm_1.createJmxConnectorServer(9878);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
 		long runFor = 60000L; // Run per default for 1 minute
 		long sleepFor = 1000L; // Check if time limit reached every second
-		long begin = System.currentTimeMillis();
 
-		while(System.currentTimeMillis() - begin < runFor)
-			try {
-				Thread.sleep(sleepFor);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
+		// 50%
+		brjvm_1.iSleep50pcOfTheTime(runFor, sleepFor);
+		
+		// 2x 25%
+		brjvm_1.iSleep25pcOfTheTime(runFor, sleepFor);
+		brjvm_1.andMeTwoiSleep25pcOfTheTimeToo(runFor, sleepFor);
+		
 		System.out.println("Basic jvm run complete.");
+		
+		try {
+			srv.stop();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void createJmxConnectorServer(int portNr) throws IOException {
+	private JMXConnectorServer  createJmxConnectorServer(int portNr) throws IOException {
 		LocateRegistry.createRegistry(portNr);
 		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 		JMXServiceURL url = new JMXServiceURL("service:jmx:rmi://localhost/jndi/rmi://localhost:"+portNr+"/jmxrmi");
 		JMXConnectorServer svr = JMXConnectorServerFactory.newJMXConnectorServer(url, null, mbs);
 		svr.start();
+		
+		return svr;
+	}
+	
+	private void iSleep(long howLong, long checkInterval){
+
+		long begin = System.currentTimeMillis();
+
+		while(System.currentTimeMillis() - begin < howLong)
+			try {
+				Thread.sleep(checkInterval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	}
 
+	private void iSleep50pcOfTheTime(long howLong, long checkInterval){
+		iSleep(howLong/2, checkInterval);
+	}
+	
+	private void iSleep25pcOfTheTime(long howLong, long checkInterval){
+		iSleep(howLong/4, checkInterval);
+	}
+	
+	private void andMeTwoiSleep25pcOfTheTimeToo(long howLong, long checkInterval){
+		iSleep(howLong/4, checkInterval);
+	}
+	
 }
