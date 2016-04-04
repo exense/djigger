574d3585b55554227937fddce8d932deec1e6835
Jérôme Comte <je.comte@gmail.com>
2016-04-04 10:39:07 +0200

    Fixing: [Client] Unexpected Character exceptions are back (very high prio) #35

--- client-ui/src/main/java/io/djigger/aggregation/filter/FilterExpressionLexer.java
+++ client-ui/src/main/java/io/djigger/aggregation/filter/FilterExpressionLexer.java
@@ -1,20 +1,3 @@
-/*******************************************************************************
- * (C) Copyright  2016 Jérôme Comte and others.
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  Contributors:
- *    - Jérôme Comte
- *******************************************************************************/
 // $ANTLR 2.7.7 (20060906): "FilterExpression.g" -> "FilterExpressionLexer.java"$
 
     package io.djigger.aggregation.filter;
@@ -83,13 +66,14 @@ tryAgain:
 				case 'M':  case 'N':  case 'O':  case 'P':
 				case 'Q':  case 'R':  case 'S':  case 'T':
 				case 'U':  case 'V':  case 'W':  case 'X':
-				case 'Y':  case 'Z':  case 'a':  case 'b':
-				case 'c':  case 'd':  case 'e':  case 'f':
-				case 'g':  case 'h':  case 'i':  case 'j':
-				case 'k':  case 'l':  case 'm':  case 'n':
-				case 'o':  case 'p':  case 'q':  case 'r':
-				case 's':  case 't':  case 'u':  case 'v':
-				case 'w':  case 'x':  case 'y':  case 'z':
+				case 'Y':  case 'Z':  case '_':  case 'a':
+				case 'b':  case 'c':  case 'd':  case 'e':
+				case 'f':  case 'g':  case 'h':  case 'i':
+				case 'j':  case 'k':  case 'l':  case 'm':
+				case 'n':  case 'o':  case 'p':  case 'q':
+				case 'r':  case 's':  case 't':  case 'u':
+				case 'v':  case 'w':  case 'x':  case 'y':
+				case 'z':
 				{
 					mWORD(true);
 					theRetToken=_returnToken;
@@ -193,6 +177,11 @@ tryAgain:
 			case '$':
 			{
 				match('$');
+				break;
+			}
+			case '_':
+			{
+				match('_');
 				break;
 			}
 			default:
