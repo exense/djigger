e271431dc9fc3d3cbc78844feba6d1889d97e4f9
c1d <c1d@C501085.suvanet.ch>
2016-04-06 20:23:50 +0200

    Derive mongod.exe from path, and if not on path set it yourself. Created default mongodb data folder.

--- distribution/template/bin/startMongo.bat
+++ distribution/template/bin/startMongo.bat
@@ -1,7 +1,9 @@
-@echo off
-SET MONGO_PATH="D:\Tools\mongodb-win32-x86_64-enterprise-windows-64-3.2.4\bin\"
+rem @echo off
+
+SET MONGO_PATH=
+rem set your own mongo path above if the mongod.exe isn't on your PATH, for example :
+rem SET MONGO_PATH="D:\Program Files\MongoDB\Server\3.0\bin\"
 
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
 
-"%MONGO_PATH%\mongod.exe" -dbpath ..\data\mongodb > ..\log\mongod_%mydate%.log 2>&1
-
+%MONGO_PATH%mongod.exe -dbpath ..\data\mongodb > ..\log\mongod_%mydate%.log 2>&1
