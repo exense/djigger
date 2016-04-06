0f67f7a784f43ee5988fc782b0db6009c8c615d1
Jérôme Comte <je.comte@gmail.com>
2016-04-06 12:55:10 +0200

    Adding linux start scripts

---
+++ distribution/template/bin/startCollector.sh
@@ -0,0 +1,6 @@
+JAVA_PATH="/usr/sbin/jre1.8.0_77/bin"
+
+JAVA_OPTS="-DcollectorConfig=../conf/Collector.xml -DconnectionsConfig=../conf/Connections.xml"
+# Use -DconnectionsConfig=../conf/Connections.csv" if you wan't to use the CSV configuration format
+
+${JAVA_PATH}/java ${JAVA_OPTS} -cp "../lib/*" io.djigger.collector.server.Server > ../log/collector_$(date +"%s").log 2>&1
