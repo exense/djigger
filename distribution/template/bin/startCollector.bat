1b9b48d87ce859ad08bed8a33236471ec2f62e5b
dcransac <dcransac@denkbar.io>
2016-04-07 13:29:30 +0200

    small windows changes (\" didn't work so I had to use "\ at the end of examples in the JDK paths)

--- distribution/template/bin/startCollector.bat
+++ distribution/template/bin/startCollector.bat
@@ -1,6 +1,9 @@
 rem @echo off
+
+rem if java.exe isn't on your path or is too old, then set your own as follows (watch for the backslash and double quote at the end):
+rem  SET JAVA_PATH="D:\Program Files\Java\jdk1.8.0_73\jre\bin"\
 SET JAVA_PATH=
-rem SET JAVA_PATH="D:\Program Files\Java\jdk1.8.0_73\jre\bin\"
+
 
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
 
--- distribution/template/bin/startMongo.bat
+++ distribution/template/bin/startMongo.bat
@@ -1,8 +1,8 @@
 rem @echo off
 
 SET MONGO_PATH=
-rem set your own mongo path above if the mongod.exe isn't on your PATH, for example :
-rem SET MONGO_PATH="D:\Program Files\MongoDB\Server\3.0\bin\"
+rem if mongod.exe isn't on your path, then set your own as follows (watch for the backslash and double quote at the end):
+rem SET MONGO_PATH="D:\Program Files\MongoDB\Server\3.0\bin"\
 
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
 
