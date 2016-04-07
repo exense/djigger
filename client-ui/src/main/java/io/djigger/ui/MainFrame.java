f1fa5d13d7e4a6ae09ec237fd8eaec0b7010fe78
Jérôme Comte <je.comte@gmail.com>
2016-04-07 17:37:45 +0200

    Releasing R1.4.2

--- client-ui/src/main/java/io/djigger/ui/MainFrame.java
+++ client-ui/src/main/java/io/djigger/ui/MainFrame.java
@@ -81,7 +81,7 @@ public class MainFrame extends JPanel {
         }
 		UIManager.put("Button.defaultButtonFollowsFocus", Boolean.TRUE);
 		
-       	frame = new JFrame("djigger 1.4.1");
+       	frame = new JFrame("djigger 1.4.2");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1300,700));
         
