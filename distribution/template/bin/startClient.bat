48b894478268550e911b815d701b88f54498de93
Jérôme Comte <je.comte@gmail.com>
2016-04-07 09:14:40 +0200

    Extracting logback configuration

--- distribution/template/bin/startClient.bat
+++ distribution/template/bin/startClient.bat
@@ -4,7 +4,7 @@ rem SET JAVA_PATH="D:\Programs\jdk1.7.0_79\jre\bin\"
 
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
 
-SET JAVA_OPTS= 
+SET JAVA_OPTS=-Dlogback.configurationFile=logback-client.xml
 
-%JAVA_PATH%java.exe %JAVA_OPTS% -cp "..\lib\*" io.djigger.ui.MainFrame > ..\log\client_%mydate%.log 2>&1
+%JAVA_PATH%java.exe %JAVA_OPTS% -cp "..\lib\*" io.djigger.ui.MainFrame > client_%mydate%.stdout 2>&1
 
--- distribution/template/bin/startCollector.sh
+++ distribution/template/bin/startCollector.sh
@@ -1,7 +1,7 @@
 JAVA_PATH=""
 #JAVA_PATH="/usr/sbin/jre1.8.0_77/bin/"
 
-JAVA_OPTS="-DcollectorConfig=../conf/Collector.xml -DconnectionsConfig=../conf/Connections.xml"
+JAVA_OPTS="-DcollectorConfig=../conf/Collector.xml -DconnectionsConfig=../conf/Connections.xml -Dlogback.configurationFile=logback-collector.xml"
 # Use -DconnectionsConfig=../conf/Connections.csv" if you wan't to use the CSV configuration format
 
-${JAVA_PATH}java ${JAVA_OPTS} -cp "../lib/*" io.djigger.collector.server.Server > ../log/collector_$(date +"%s").log 2>&1
+${JAVA_PATH}java ${JAVA_OPTS} -cp "../lib/*" io.djigger.collector.server.Server > collector_$(date +"%s").stdout 2>&1
