a8e6f54f220d86f47463ed1ffdd0517dc640aff0
c1d <c1d@C501085.suvanet.ch>
2016-04-07 11:52:35 +0200

    fixed log path (..\log not needed anymore, although temporarly)

--- distribution/template/bin/startMongo.sh
+++ distribution/template/bin/startMongo.sh
@@ -1,3 +1,3 @@
 MONGO_PATH="/usr/bin"
 
-${MONGO_PATH}/mongod -dbpath ../data/mongodb --smallfiles > ../log/mongod_$(date +"%s").log 2>&1
+${MONGO_PATH}/mongod -dbpath ../data/mongodb --smallfiles > ./mongod_$(date +"%s").log 2>&1
