291ad2413c0989727c4e82570f4ea25f2ad06590
Jérôme Comte <je.comte@gmail.com>
2016-05-26 13:34:59 +0200

    R1.5.1

--- client-ui/src/test/java/io/djigger/test/e2e/TestApp.java
+++ client-ui/src/test/java/io/djigger/test/e2e/TestApp.java
@@ -1,8 +1,32 @@
 package io.djigger.test.e2e;
 
-public class TestApp {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public abstract class  TestApp {
+
+	static Connection conn= null;
+	
+	
+	private interface TEst {
+		
+	}
+	
+	private abstract class Test2 implements TEst {}
+	
+	private class Test3 extends Test2 {}
+	
 	public static void main(String[] args) {
+		
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:hsqldb:mem:mymemdb", "SA", "");
+			conn.createStatement().execute("CREATE TABLE test (att1 VARCHAR(10))");
+		} catch (SQLException e) {
+		}
+		
 		(new Thread(new Runnable() {
 			
 			@Override
@@ -42,6 +66,9 @@ public class TestApp {
 	public static void testMethod1() {
 		testMethod2_1();
 		testMethod2_2();
+		testMethod2_3();
+		testMethod2_4();
+		testMethod_SQL();
 	}
 	
 	public static void testMethod2_1() {
@@ -50,6 +77,46 @@ public class TestApp {
 	
 	public static void testMethod2_2() {
 		testMethod1ms();
+	}
+	
+	public static String testMethod2_3() {
+		testMethod1ms();
+		return "testMethod2_3_result";
+	}
+	
+	public static long testMethod2_4() {
+		testMethod1ms();
+		return 1;
+	}
+	
+	public static long testMethod_SQL() {
+		try {
+			Class.forName("org.hsqldb.jdbc.JDBCDriver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		PreparedStatement statement = null;
+		try {
+			statement = conn.prepareStatement("select * from test");
+			//statement.execute();
+			statement.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				statement.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		
+		testMethod1ms();
+		return 1;
 	}
 	
 	
