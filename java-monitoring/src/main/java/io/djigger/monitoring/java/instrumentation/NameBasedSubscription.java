678c2ba4d6f77dcdac0686a3e716a6c02eb865b4
Jérôme Comte <jcomte@denkbar.io>
2016-11-15 13:14:33 +0100

    Removing project java-monitoring

--- java-monitoring/src/main/java/io/djigger/monitoring/java/instrumentation/NameBasedSubscription.java
+++
@@ -1,8 +0,0 @@
-package io.djigger.monitoring.java.instrumentation;
-
-public interface NameBasedSubscription {
-
-	public boolean isRelatedToClass(String classname);
-	
-	public boolean isRelatedToMethod(String method);
-}
