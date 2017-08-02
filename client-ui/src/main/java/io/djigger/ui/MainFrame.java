c86228877c2203ed79607af19271617e5173d4bd
Jérôme Comte <jcomte@denkbar.io>
2017-08-02 08:10:51 +0200

    Bumping to 1.7.1

--- client-ui/src/main/java/io/djigger/ui/MainFrame.java
+++ client-ui/src/main/java/io/djigger/ui/MainFrame.java
@@ -86,7 +86,7 @@ public class MainFrame extends JPanel {
         }
 		UIManager.put("Button.defaultButtonFollowsFocus", Boolean.TRUE);
 		
-       	frame = new JFrame("djigger 1.7.0");
+       	frame = new JFrame("djigger 1.7.1");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1300,700));
         
