e39dc5e45c76a30e1f4263cd19208dcb57c8805a
Reto Gantenbein <reto.gantenbein@linuxmonk.ch>
2017-11-08 18:36:42 +0100

    Set DJIGGER_HOME in startup scripts and make it more robust

--- distribution/template/bin/startClient.sh
+++ distribution/template/bin/startClient.sh
@@ -1,6 +1,15 @@
-JAVA_PATH=""
-#JAVA_PATH="/usr/sbin/jre1.8.0_77/bin/"
+#!/bin/bash
 
-JAVA_OPTS=-Dlogback.configurationFile=logback-client.xml
+execdir="$(readlink -f `dirname ${BASH_SOURCE[0]}`)"
 
-${JAVA_PATH}java ${JAVA_OPTS} -cp "../lib/*" io.djigger.ui.MainFrame
+DJIGGER_HOME="${DJIGGER_HOME:-$(dirname ${execdir})}"
+DJIGGER_CONFDIR="${DJIGGER_CONFDIR:-${DJIGGER_HOME}/conf}"
+DJIGGER_LIBDIR="${DJIGGER_LIBDIR:-${DJIGGER_HOME}/lib}"
+
+START_OPTS=()
+START_OPTS+=("-Dlogback.configurationFile=${DJIGGER_CONFDIR}/logback-client.xml")
+START_OPTS+=("${JAVA_OPTS}")
+
+cd "${DJIGGER_HOME}" \
+    && exec "${JAVA_HOME}java" ${START_OPTS[@]} -cp "${DJIGGER_LIBDIR}/*" io.djigger.ui.MainFrame \
+    || echo "Error: Invalid DJIGGER_HOME"
