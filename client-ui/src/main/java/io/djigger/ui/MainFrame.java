32fcbb95f0fa68f0d5c7b15cadbee414eb7f5688
Jérôme Comte <jerome.comte@exense.ch>
2018-02-12 16:18:29 +0100

    Bumping 1.8.2

--- client-ui/src/main/java/io/djigger/ui/MainFrame.java
+++ client-ui/src/main/java/io/djigger/ui/MainFrame.java
@@ -86,7 +86,7 @@ public class MainFrame extends JPanel {
         }
 		UIManager.put("Button.defaultButtonFollowsFocus", Boolean.TRUE);
 		
-       	frame = new JFrame("djigger 1.8.1");
+       	frame = new JFrame("djigger 1.8.2");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1300,700));
         
