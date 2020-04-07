c491155270a491301ae6d927099f7725bb78c900
David Stephan <David.Stephan@exense.ch>
2020-04-07 12:08:08 +0200

    Bumping version to 1.10.2

--- client-ui/src/main/java/io/djigger/ui/MainFrame.java
+++ client-ui/src/main/java/io/djigger/ui/MainFrame.java
@@ -76,7 +76,7 @@ public class MainFrame extends JPanel {
         }
         UIManager.put("Button.defaultButtonFollowsFocus", Boolean.TRUE);
 
-        frame = new JFrame("djigger 1.10.1");
+        frame = new JFrame("djigger 1.10.2");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1300, 700));
 
