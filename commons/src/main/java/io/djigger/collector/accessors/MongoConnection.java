82c60eff4584ede3ed399bbe9c0a9fc0a7c5f2d9
David Stephan <David.Stephan@exense.ch>
2020-06-30 15:36:51 +0200

    #55 implemented connection retry to mongo at startup (max 10 minutes)

--- commons/src/main/java/io/djigger/collector/accessors/MongoConnection.java
+++ commons/src/main/java/io/djigger/collector/accessors/MongoConnection.java
@@ -1,20 +1,30 @@
 package io.djigger.collector.accessors;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientOptions.Builder;
 import com.mongodb.MongoCredential;
+import com.mongodb.MongoSocketOpenException;
+import com.mongodb.MongoTimeoutException;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MongoConnection {
+	
+    private static final Logger logger = LoggerFactory.getLogger(MongoConnection.class);
 
     private MongoClient mongoClient;
 
     private MongoDatabase db;
+    
+    private static int MAX_RETRIES = 60;
 
     public MongoDatabase connect(String host, int port, String user, String password) {
         Builder o = MongoClientOptions.builder().serverSelectionTimeout(3000);
@@ -29,8 +39,17 @@ public class MongoConnection {
         mongoClient = new MongoClient(new ServerAddress(host, port), credentials, o.build());
 
         // call this method to check if the connection succeeded as the mongo client lazy loads the connection
-        mongoClient.getAddress();
-
+        boolean isConnected = false;
+        int tries = 0;
+        while (!isConnected && tries < MAX_RETRIES) {
+	        try {
+	        	tries++;
+	        	mongoClient.getAddress();
+	        	isConnected = true;
+	        } catch (MongoSocketOpenException | MongoTimeoutException e) {
+	        	logger.warn("Unable to establish a connection to the mongo DB, retrying in 10 seconds...");
+	        }
+        }
         db = mongoClient.getDatabase(databaseName);
         return db;
     }
