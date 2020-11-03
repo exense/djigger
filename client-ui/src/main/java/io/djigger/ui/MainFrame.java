e6641597bb4565b0307d68a4a9af84046dfd1210
David Stephan <David.Stephan@exense.ch>
2020-11-03 11:59:08 +0100

    Update GUI version to 1.12.0

--- client-ui/src/main/java/io/djigger/ui/MainFrame.java
+++ client-ui/src/main/java/io/djigger/ui/MainFrame.java
@@ -76,7 +76,7 @@ public class MainFrame extends JPanel {
         }
         UIManager.put("Button.defaultButtonFollowsFocus", Boolean.TRUE);
 
-        frame = new JFrame("djigger 1.11.1");
+        frame = new JFrame("djigger 1.12.0");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1300, 700));
 
