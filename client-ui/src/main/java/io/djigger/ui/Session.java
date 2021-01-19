97571c0e799cbe82639f157c7034cc93ed07b2f8
David Stephan <David.Stephan@exense.ch>
2021-01-19 12:09:30 +0100

    re-apply metrics filter when duplicating session
    (cherry picked from commit 3e973082a761d6026e1ddf773dd1ec302fa24e3e)

--- client-ui/src/main/java/io/djigger/ui/Session.java
+++ client-ui/src/main/java/io/djigger/ui/Session.java
@@ -508,6 +508,7 @@ public class Session extends JPanel implements FacadeListener, Closeable {
         reloadStatisticsCache();
 
         threadSelectionPane.refresh();
+        threadSelectionPane.selectionChanged();
         refreshAnalyzerGroupPane();
     }
 
